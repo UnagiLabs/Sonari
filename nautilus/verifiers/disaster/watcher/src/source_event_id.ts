94da2223cc98768c4d643ee97ecc806076a7e792
bu-bu-BUTASAN <bububutasan00@gmail.com>
2026-05-25 05:22:35 +0000

    fix: hyphen入りsource_event_idを許可する

--- nautilus/verifiers/disaster/watcher/src/source_event_id.ts
+++ nautilus/verifiers/disaster/watcher/src/source_event_id.ts
@@ -1,4 +1,4 @@
-const USGS_SOURCE_EVENT_ID_PATTERN = /^[a-z]{2}[a-z0-9]{6,32}$/;
+const USGS_SOURCE_EVENT_ID_PATTERN = /^[a-z]{2}[a-z0-9-]{6,32}$/;
 
 export function isValidUsgsSourceEventId(sourceEventId: string): boolean {
     return USGS_SOURCE_EVENT_ID_PATTERN.test(sourceEventId);
